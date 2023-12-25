8f82b7e342049140a3c56b329d5abc6835cf5e9d
Gaurav Tak <gaurav.tak@roro.io>
2023-12-26 01:22:35 +0530

    Initial Commit

---
+++ ToastViewWithSwiftUI/ToastViewWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastViewWithSwiftUIApp.swift
+//  ToastViewWithSwiftUI
+//
+//  Created by Roro Solutions LLP on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ToastViewWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
